10ba96850864d7cb1f6a29815732174da6c8e54a
Raluca211 <raluca_ciobanu211@yahoo.com>
2016-04-18 16:48:54 +0300

    lab4
    lab4

---
+++ MoviesACLabs/Scripts/lab4.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('Start', []);
+
+
+app.controller('add', function ($scope) {
+
+    $scope.todos = ['bhg', 'jhj'];
+    $scope.todoList = "";
+   
+
+   
+    $scope.addTodo = function () {
+        $scope.todos.push($scope.todoList);
+    }
+
+    $scope.submit = function() {
+        $scope.todos.push($scope.todoList);
+        }
+   
+
+
+});
